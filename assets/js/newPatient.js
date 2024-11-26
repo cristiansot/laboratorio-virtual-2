d6818ac1cb34c1d20711541149e5f5cc81eea7f7
cristiansot <soluciongrafix@gmail.com>
2024-11-25 21:30:58 -0300

    Se implementa dispara de nuevo paciente

---
+++ assets/js/newPatient.js
@@ -0,0 +1,20 @@
+function simulateNewPatient() {
+  const patient = "Juan Perez";
+
+  const event = new CustomEvent("newPatient", { detail: patient });
+  document.dispatchEvent(event);
+}
+
+document.addEventListener("newPatient", (e) => {
+  const patient = e.detail;
+
+  const notificationArea = document.getElementById("notification-area");
+  notificationArea.innerHTML = `
+    <div class="alert alert-success alert-dismissible fade show mb-0" role="alert">
+      <strong>Nuevo paciente recibido:</strong> ${patient}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `;
+});
+
+setTimeout(simulateNewPatient, 2000);
